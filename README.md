37b173ce4fbfa1f38f06926f515d4ee4d08f2f16
Damian <damianobattaglia97@gmail.com>
2022-12-17 09:50:21 +0100

    removed unnecessary files from create-react-app install

---
+++ README.md
@@ -0,0 +1,1 @@
+I got to the final javascript course project! I can already feel the sweat on my brow, but I know I can pull this off. 
---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      Hello From React!
+    </div>
+  );
+}
+
+export default App;
