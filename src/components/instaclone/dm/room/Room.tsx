b3eeeaa7ab60bb286726e60de21dc117ce76e969
Velthas <turquoisescar@gmail.com>
2023-05-19 09:32:28 +0200

    feat: add enter event to send messages

--- src/components/instaclone/dm/room/Room.tsx
+++ src/components/instaclone/dm/room/Room.tsx
@@ -3,7 +3,10 @@ import styled from "styled-components";
 import { useNavigate, useParams } from "react-router-dom";
 import { IconContext } from "react-icons";
 import { getCurrentUserUsername } from "../../../../firebase/authentication";
-import { addMessage, setupMessagesListener} from "../../../../firebase/firestore";
+import {
+  addMessage,
+  setupMessagesListener,
+} from "../../../../firebase/firestore";
 import { flexColumnCenter } from "../../../../styles/style";
 import { formatMessage } from "../../../../utils/formatting";
 import { ChatMessage, Chatroom } from "../../../../utils/types";
@@ -13,12 +16,12 @@ import RoomHeader from "./RoomHeader";
 import Textbox from "./Textbox";
 
 type Props = {
-  active: Chatroom | null,
-  setActive: (room: Chatroom | null) => void,
-  setNewRoom: (room: Chatroom | null) => void,
-  newRoom : Chatroom | null,
-  rooms: Chatroom[] | null,
-}
+  active: Chatroom | null;
+  setActive: (room: Chatroom | null) => void;
+  setNewRoom: (room: Chatroom | null) => void;
+  newRoom: Chatroom | null;
+  rooms: Chatroom[] | null;
+};
 
 const Room = ({ active, setActive, setNewRoom, newRoom, rooms }: Props) => {
   const user = getCurrentUserUsername() as string;
@@ -30,7 +33,7 @@ const Room = ({ active, setActive, setNewRoom, newRoom, rooms }: Props) => {
   // Handles subscribing to chat on db for message updates
   useEffect(() => {
     if (unsubscribe.current) unsubscribe.current();
-    if(id) unsubscribe.current = setupMessagesListener(id, setMessages);
+    if (id) unsubscribe.current = setupMessagesListener(id, setMessages);
     return () => {
       if (unsubscribe.current) unsubscribe.current();
     };
@@ -39,17 +42,29 @@ const Room = ({ active, setActive, setNewRoom, newRoom, rooms }: Props) => {
   // Handles scrolling to bottom of chat when messages are loaded
   useEffect(() => {
     var objDiv = document.getElementById("message-box");
-    if(objDiv) objDiv.scrollTop = objDiv.scrollHeight;
+    if (objDiv) objDiv.scrollTop = objDiv.scrollHeight;
   }, [messages]);
 
   // Handles setting correct chat as active when ID changes
   useEffect(() => {
-    if (rooms && active && rooms.some((room) => (room.chatId === id && (room.chatId === active.chatId || (newRoom && newRoom.chatId === room.chatId)))))
+    if (
+      rooms &&
+      active &&
+      rooms.some(
+        (room) =>
+          room.chatId === id &&
+          (room.chatId === active.chatId ||
+            (newRoom && newRoom.chatId === room.chatId))
+      )
+    )
       return;
     else if (rooms) {
       const activeRoom = rooms.filter((room) => room.chatId === id)[0];
-      const payload = { username: activeRoom.username, chatId: activeRoom.chatId };
-      if(!activeRoom.lastMessage) setNewRoom(payload);
+      const payload = {
+        username: activeRoom.username,
+        chatId: activeRoom.chatId,
+      };
+      if (!activeRoom.lastMessage) setNewRoom(payload);
       setActive(payload);
     }
   }, [rooms, id]);
@@ -57,13 +72,18 @@ const Room = ({ active, setActive, setNewRoom, newRoom, rooms }: Props) => {
   // Handles sending message to backend
   const sendMessage = () => {
     const messageBox = document.querySelector<HTMLInputElement>("#add-message");
-    if(!messageBox) return;
+    if (!messageBox) return;
     const message = messageBox.value;
     if (message.length > 2200 || message.length < 0) return;
     const formattedMessage = formatMessage(message);
-    if(user && active && id && formattedMessage.author) 
+    if (user && active && id && formattedMessage.author)
       addMessage(user, active.username, id, formattedMessage);
     messageBox.value = "";
+  };
+
+  // Sends message when user presses enter
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === "Enter") sendMessage();
   };
 
   // Used to backtrack to chat selection in mobile view
@@ -73,8 +93,8 @@ const Room = ({ active, setActive, setNewRoom, newRoom, rooms }: Props) => {
   };
 
   return (
-    <IconContext.Provider value={{ style: { cursor: "pointer" }, size: '24' }}>
-      <Container>
+    <IconContext.Provider value={{ style: { cursor: "pointer" }, size: "24" }}>
+      <Container onKeyPress={(e) => handleKeyPress(e)}>
         <RoomHeader backToChatSelection={backToChatSelection} active={active} />
         <MessagesContainer id={"message-box"}>
           {messages &&
